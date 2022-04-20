7359a11d83ba6691529697e602497e31c7fda41c
faizdamar1 <faizdamar123@gmail.com>
2022-04-20 12:36:04 +0700

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,33 +35,187 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Container(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              const Text(
-                "Verification Code",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+      appBar: AppBar(backgroundColor: Colors.white, elevation: 0),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                const Text(
+                  "Verification Code",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              RichText(
-                text: TextSpan(
-                  text: 'We have sent the code verification to +62*******8301 ',
-                  style: const TextStyle(color: Colors.black, fontSize: 18),
-                  children: <TextSpan>[
-                    TextSpan(
-                        text: 'Change pthe phone number?',
-                        style: const TextStyle(
-                            color: Colors.blueAccent, fontSize: 18),
-                        recognizer: TapGestureRecognizer()..onTap = () {})
+                const SizedBox(
+                  height: 20,
+                ),
+                RichText(
+                  text: TextSpan(
+                    text:
+                        'We have sent the code verification to +62*******8301 ',
+                    style: const TextStyle(color: Colors.black, fontSize: 18),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'Change the phone number?',
+                          style:
+                              TextStyle(color: Colors.blue[900], fontSize: 18),
+                          recognizer: TapGestureRecognizer()..onTap = () {})
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 65,
+                      height: 64,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(width: 1, color: Colors.grey)),
+                      child: TextField(
+                        onChanged: (value) {
+                          if (value.length == 1) {
+                            FocusScope.of(context).nextFocus();
+                          }
+                        },
+                        style: Theme.of(context).textTheme.headline6,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(1),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 65,
+                      height: 64,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(width: 1, color: Colors.grey)),
+                      child: TextField(
+                        onChanged: (value) {
+                          if (value.length == 1) {
+                            FocusScope.of(context).nextFocus();
+                          }
+                        },
+                        style: Theme.of(context).textTheme.headline6,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(1),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 65,
+                      height: 64,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(width: 1, color: Colors.grey)),
+                      child: TextField(
+                        onChanged: (value) {
+                          if (value.length == 1) {
+                            FocusScope.of(context).nextFocus();
+                          }
+                        },
+                        style: Theme.of(context).textTheme.headline6,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(1),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 65,
+                      height: 64,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(width: 1, color: Colors.grey)),
+                      child: TextField(
+                        onChanged: (value) {
+                          if (value.length == 1) {
+                            FocusScope.of(context).nextFocus();
+                          }
+                        },
+                        style: Theme.of(context).textTheme.headline6,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(1),
+                        ],
+                      ),
+                    ),
                   ],
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(width: 1, color: Colors.grey),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Resend",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue[900]),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ));
+                const SizedBox(width: 5),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                    decoration: BoxDecoration(
+                      color: Colors.blue[900],
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(width: 1, color: Colors.blue),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Confirm",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
